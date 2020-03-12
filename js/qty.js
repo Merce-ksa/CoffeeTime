5e44d3efff9393306215f568c2d87bed9724e1ed
Merce Casamitjana <casamitjana.agraz@gmail.com>
2020-03-12 14:51:40 +0100

    version 1

---
+++ js/qty.js
@@ -0,0 +1,39 @@
+var check = false;
+
+function changeVal(el) {
+  var qt = parseFloat(el.parent().children(".qt").html());
+  var price = parseFloat(el.parent().children(".price").html());
+  var eq = Math.round(price * qt * 100) / 100;
+
+  el.parent().children(".full-price").html( eq + " €" );
+
+  changeTotal();
+}
+
+$(document).ready(function(){
+
+  $(".qt-plus").click(function(){
+    $(this).parent().children(".qt").html(parseInt($(this).parent().children(".qt").html()) + 1);
+
+    $(this).parent().children(".full-price").addClass("added");
+
+    var el = $(this);
+    window.setTimeout(function(){el.parent().children(".full-price").removeClass("added"); changeVal(el);}, 150);
+  });
+
+  $(".qt-minus").click(function(){
+
+    child = $(this).parent().children(".qt");
+
+    if(parseInt(child.html()) > 1) {
+      child.html(parseInt(child.html()) - 1);
+    }
+
+    $(this).parent().children(".full-price").addClass("minused");
+
+    var el = $(this);
+    window.setTimeout(function(){el.parent().children(".full-price").removeClass("minused"); changeVal(el);}, 150);
+  });
+
+  window.setTimeout(function(){$(".is-open").removeClass("is-open")}, 1200);
+});
